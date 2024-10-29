f7ea6bca76db27dbe6a73801fbbad5464cb4367d
bryant.graham@gmail.com <bryant.graham@gmail.com>
2024-10-29 14:39:54 +0000

    dim_customer refactor

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -1,12 +1,18 @@
 with customers as (
 
-     select * from {{ ref('stg_jaffle_shop__customers') }}
+    select * from {{ ref('stg_jaffle_shop__customers') }}
 
 ),
 
-orders as ( 
+orders as (
 
     select * from {{ ref('stg_jaffle_shop__orders') }}
+
+),
+
+fct_orders as (
+
+    select * from {{ ref('fct_orders') }}
 
 ),
 
@@ -25,6 +31,16 @@ customer_orders as (
 
 ),
 
+customer_ltv as (
+
+    select
+        customer_id,
+        sum(amount) as lifetime_value
+
+    from fct_orders
+    group by 1
+),
+
 final as (
 
     select
@@ -33,12 +49,16 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce (customer_orders.number_of_orders, 0) 
-        as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0)
+            as number_of_orders,
+        coalesce(customer_ltv.lifetime_value, 0) as lifetime_value
 
     from customers
 
-    left join customer_orders using (customer_id)
+    left join
+        customer_orders
+        on customers.customer_id = customer_orders.customer_id
+left join customer_ltv using (customer_id)
 
 )
 
